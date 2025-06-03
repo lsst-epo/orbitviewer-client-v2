ec2f039aa889bdcdd61c50f23d0ff88c34d0ef6f
kulu <sindiploma@gmail.com>
2025-06-03 16:20:14 +0200

    WIP.

--- data/copy.js
+++ data/copy.js
@@ -1,10 +1,30 @@
 export default {
+    "logo": {
+        "alt": {
+            "en": "NSF DOE Rubin Observatory Orbitviewer logo",
+            "es": ""
+        }
+    },
     "home": {
+        "logo": {
+            "alt": {
+                "en": "NSF-DOE Vera C. Rubin Observatory logo",
+                "es": ""
+            }
+        },
         "discoveries": {
             "en": "Discoveries Unveiled",
-            "es": "Descubrisiones"
+            "es": ""
         },
         "languages": {
+            "english": {
+                "shorthand": "en",
+                "full": "english"
+            },
+            "spanish": {
+                "shorthand": "es",
+                "full": "español"
+            }
         }
-    }
+    },
 }
