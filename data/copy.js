b79e5537ffb00d2107312dbc301ca01b7fe4eef5
kulu <sindiploma@gmail.com>
2025-06-03 18:13:58 +0200

    Canvas pointers.

--- data/copy.js
+++ data/copy.js
@@ -19,12 +19,58 @@ export default {
         "languages": {
             "english": {
                 "shorthand": "en",
-                "full": "english"
+                "full": "English"
             },
             "spanish": {
                 "shorthand": "es",
-                "full": "español"
+                "full": "Español"
             }
         }
     },
+    "object": {
+        "hero": {
+            "alt": {
+                "en": "Astronomical image of",
+                "es": ""
+            }
+        },
+        "diameter": {
+            "en": "Diameter",
+            "es": ""
+        },
+        "orbital_elements": {
+            "title": {
+                "en": "Orbital elements",
+                "es": ""
+            },
+            "label": {
+                "en": "Orbital element parameters",
+                "es": ""
+            },
+            "periapsis": {
+                "en": "Argument of periapsis",
+                "es": ""
+            },
+            "longitude": {
+                "en": "Longitude of the ascending node",
+                "es": ""
+            },
+            "anomaly": {
+                "en": "True anomaly",
+                "es": ""
+            }
+        },
+        "closest_sun": {
+            "title": {
+                "en": "When it will be closest to the Sun?",
+                "es": ""
+            }
+        },
+        "how_far": {
+            "title": {
+                "en": "How far is it today?",
+                "es": ""
+            }
+        }
+    }
 }
