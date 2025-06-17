432c5d196f943189d4b546d284e72a36e88bf440
kulu <sindiploma@gmail.com>
2025-06-17 18:48:51 +0200

    About - WIP.

--- src/js/core/App.ts
+++ src/js/core/App.ts
@@ -49,7 +49,7 @@ export class App implements NomadRouteListener {
 		GLOBALS.loader = new Loader(document.querySelector('.loader'));
 		GLOBALS.loader.show();
 
-		document.body.style.overflow = 'hidden';
+		// document.body.style.overflow = 'hidden';
 
 		initShaders();
 
