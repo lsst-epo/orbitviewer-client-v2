1acae3a361d28fc5165fcd1f308ccae13dc66f9b
Christian MacMillan <christian@macmillan.studio>
2025-06-19 16:19:06 +0200

    Share

--- src/js/layers/Share.ts
+++ src/js/layers/Share.ts
@@ -2,14 +2,22 @@ import Tabs from "../components/Tabs";
 import ToggleGroup from "../components/ToggleGroup";
 import Layer from "./Layer";
 import { copyToClipboard } from "@fils/utils";
+import { ShareAPI } from "../core/ShareAPI";
 
 class Share extends Layer {
     dom: HTMLElement;
     triggerButton: HTMLElement;
+    input: HTMLInputElement;
     closeButton: HTMLElement;
     ratioToggle: HTMLElement;
     copyButton: HTMLElement;
     
+    fbButton: HTMLElement;
+    xButton: HTMLElement;
+    apiButton: HTMLElement;
+
+    shareAPI : ShareAPI = new ShareAPI();
+
     constructor(dom) {
         super(dom, {
             openClass: 'share--open',
@@ -23,6 +31,12 @@ class Share extends Layer {
         this.triggerButton = document.querySelector('.button_share');
         this.ratioToggle = dom.querySelector('#toggle-ratio');
         this.copyButton = dom.querySelector('.button_copy');
+
+        this.fbButton = dom.querySelector('.button-fb');
+        this.xButton = dom.querySelector('.button-x');
+        this.apiButton = dom.querySelector('.button-api');
+
+        this.input = dom.querySelector('input[type="url"]') as HTMLInputElement;
 
         this.start();
     }
@@ -38,16 +52,33 @@ class Share extends Layer {
 			this.close();
 		});
 
+        this.fbButton.addEventListener('click', (e) => {
+            e.preventDefault();
+            this.shareAPI.facebook(this.getInputValue());
+        });
+
+        this.apiButton.addEventListener('click', (e) => {
+            e.preventDefault();
+            this.shareAPI.share({
+                title: '', // You can set a title if needed
+                text: '', // You can set a text if needed
+                url: this.getInputValue()
+            });
+        });
+
+        this.xButton.addEventListener('click', (e) => {
+            e.preventDefault();
+            this.shareAPI.x(this.getInputValue());
+        });
+
         this.copyButton.addEventListener('click', (e) => {
-            const input = this.copyButton.closest('div').querySelector('input');
-            if (!input || this.copyButton.classList.contains('active')) return; // Prevent multiple clicks or if input is not found
-            copyToClipboard(input.value);
+            e.preventDefault();
+            if (this.copyButton.classList.contains('active')) return; // Prevent multiple clicks or if input is not found
+            copyToClipboard(this.getInputValue());
             this.copyButton.classList.add('active');
             setTimeout(() => {
-                // TODO: SHOW Active THREE SECONDS
                 this.copyButton.classList.remove('active');
             }, 3000); // Reset the button after 3 seconds
-            
         })
         
 		const shareTabs = new Tabs('.share_dialog-body');
@@ -64,6 +95,11 @@ class Share extends Layer {
                 }                
             }
         });
+    }
+
+    getInputValue() {
+        if (!this.input) return '';
+        return this.input.value.trim();
     }
 }
 
