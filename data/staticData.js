47d044c0d6dce12388aae78189ffd0e332553573
Edu Prats <eduprats@gmail.com>
2025-06-21 14:55:46 +0200

    Added SEO images

--- data/staticData.js
+++ data/staticData.js
@@ -2,10 +2,12 @@ export default {
 	// Get global "isProduction"
 	isProduction: process.env.ELEVENTY_ENV === 'production',
 	baseURL: "",
-  title: "Orbit Viewer Engine V2",
+  title: "Orbit Viewer",
 	description: "Vera Rubin's interactive explorer.",
 	languages: ["en", "es"],
 	defaultLanguage: "en",
+	rootDomain: "https://d251spak4lbyvj.cloudfront.net/en/",
+	stagingDomain: "https://d251spak4lbyvj.cloudfront.net/",
 	locale: {
     en: "en",
 		es: "es"
