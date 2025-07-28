d9d6ba08ae9d721d9ea8f34f82ae3118c624fa5a
Kevin Wong <kevin.wong@noirlab.edu>
2025-07-28 16:22:26 -0700

    adds more comments

--- src/env.js
+++ src/env.js
@@ -1,5 +1,5 @@
 export const env = {
-    PROD_TOKEN: process.env.PROD_TOKEN ?? "missing_prod_token", // TODO: remove this prod token from version 
+    PROD_TOKEN: process.env.PROD_TOKEN ?? "missing_prod_token", // TODO: remove the hard-coded prod token from version control
     URL: process.env.URL ?? 'https://orbitviewer-api-dot-skyviewer.uw.r.appspot.com'
 }
 
