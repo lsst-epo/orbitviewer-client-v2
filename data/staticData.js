c74f284071414f208293ce062aa13e0d6dfe3db9
Edu Prats <eduprats@gmail.com>
2025-09-04 15:16:11 +0200

    language integration fixes

--- data/staticData.js
+++ data/staticData.js
@@ -9,7 +9,7 @@ export default {
 	rootDomain: "https://orbitviewer.app/",
 	stagingDomain: "https://d251spak4lbyvj.cloudfront.net/",
 	og: {
-		basePath: 'assets/images/og/',
+		basePath: '/assets/images/og/',
 		default: 'default.webp'
 	},
 	locale: {
