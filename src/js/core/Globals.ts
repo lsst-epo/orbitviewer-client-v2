7e6a951b73c955c56a9b1e24f23a0d2d38cc80fe
Eric Rosas <ericdrosas@gmail.com>
2026-04-03 16:37:14 -0700

    fix: added hasura key as env var

--- src/js/core/Globals.ts
+++ src/js/core/Globals.ts
@@ -21,12 +21,9 @@ import { OrbitDataElementsV2 } from "./solar/SolarUtils";
  */
 //@ts-ignore
 export const IS_DEV_MODE = DEV_MODE;
-// export const TARGET = TARGET_MODE;
-
-// export const HASURA_URL = `https://hasura-e3g4rcii3q-uc.a.run.app/api/rest`;
-export const HASURA_URL = `https://hasura-688095955960.us-central1.run.app/api/rest`;
-export const HASURA_GRAPHQL = `https://hasura-688095955960.us-central1.run.app/v1/graphql`;
-
+export const HASURA_URL = process.env.HASURA_URL;
+export const HASURA_GRAPHQL = process.env.HASURA_GRAPHQL;
+export const SECRET_KEY = process.env.HASURA_SECRET_KEY;
 
 export const PATHS = {
 	uploads: '/uploads',
